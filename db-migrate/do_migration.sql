c1637acf8013b6a5df06dc88a131ffe8df0bccfc
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-02-10 03:57:10 +0000

    refine a bit

--- db-migrate/do_migration.sql
+++ db-migrate/do_migration.sql
@@ -7,11 +7,11 @@ BEGIN
 END;
 /
 
-@ ./do/1-do_tbl_version
-@ ./do/2-do_tbl_version_change_log
-@ ./do/3-do_seq_version_change_log
-@ ./do/4-do_sp_update_version
-@ ./do/5-do_sp_log_version_change_begin
-@ ./do/6-do_sp_log_version_change_finish
-@ ./do/7-do_comment_dm_vbpa
+@@ do/1-do_tbl_version
+@@ do/2-do_tbl_version_change_log
+@@ do/3-do_seq_version_change_log
+@@ do/4-do_sp_update_version
+@@ do/5-do_sp_log_version_change_begin
+@@ do/6-do_sp_log_version_change_finish
+@@ do/7-do_comment_dm_vbpa
 
--- db-migrate/undo_migration.sql
+++ db-migrate/undo_migration.sql
@@ -7,12 +7,12 @@ BEGIN
 END;
 /
 
-@ ./undo/7-undo_comment_dm_vbpa :migrate_user
-@ ./undo/6-undo_sp_log_version_change_finish
-@ ./undo/5-undo_sp_log_version_change_begin
-@ ./undo/4-undo_sp_update_version
-@ ./undo/3-undo_seq_version_change_log
-@ ./undo/2-undo_tbl_version_change_log
-@ ./undo/1-undo_tbl_version :migrate_user
+@@ undo/7-undo_comment_dm_vbpa :migrate_user
+@@ undo/6-undo_sp_log_version_change_finish
+@@ undo/5-undo_sp_log_version_change_begin
+@@ undo/4-undo_sp_update_version
+@@ undo/3-undo_seq_version_change_log
+@@ undo/2-undo_tbl_version_change_log
+@@ undo/1-undo_tbl_version :migrate_user
 
 
