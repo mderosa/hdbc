efc972a8cf82baa835c3a9527f0ad9e553fe8db0
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-02-10 00:10:43 +0000

    get these compiling

--- trunk/db-migrate/src/main/java/com/googlecode/hdbc/dbmigrate/DbMigrate.java
+++ trunk/db-migrate/src/main/java/com/googlecode/hdbc/dbmigrate/DbMigrate.java
@@ -2,8 +2,13 @@ package com.googlecode.hdbc.dbmigrate;
 
 import static com.googlecode.hdbc.dbmigrate.Menu.*;
 import static com.googlecode.hdbc.dbmigrate.MenuItem.*;
+
+import java.io.IOException;
+
+import com.googlecode.hdbc.dbmigrate.processor.TopLevelMenuResponseProcessor;
 import com.googlecode.hdbc.dbmigrate.processor.NullProcessor;
 import com.googlecode.hdbc.dbmigrate.validator.NullValidator;
+import com.googlecode.hdbc.dbmigrate.validator.TopLevelMenuResponseValidator;
 
 public final class DbMigrate {
 
@@ -12,9 +17,9 @@ public final class DbMigrate {
     }
 
     public static void main(final String[] args) {
-        Menu menu = menu(processWith(NullProcessor.class), vaidateWith(NullValidator.class))
+        Menu menu = menu(processWith(TopLevelMenuResponseProcessor.class), validateWith(TopLevelMenuResponseValidator.class))
                 .add(item("1. Create a new do/undo script from a template",
-                        menu(processWith(NullProcessor.class), vaidateWith(NullValidator.class))
+                        menu(processWith(NullProcessor.class), validateWith(NullValidator.class))
                         .add(item("Enter the name of the migration script:",
                                 menu(processWith(NullProcessor.class), validateWith(NullValidator.class))
                                 .add(item("Do you wish to create a DDL script or a PL/SQL DML script (DDL/DML):", null))))))
@@ -23,7 +28,11 @@ public final class DbMigrate {
                         .add(item("Enter the current database version (Enter 0 if the db is currently unversioned):",
                                 menu(processWith(NullProcessor.class), validateWith(NullValidator.class))
                                 .add(item("Enter the version that you wish to migrate to:", null))))));
-        menu.run();
+        try {
+            menu.run();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
 }
--- trunk/db-migrate/src/main/java/com/googlecode/hdbc/dbmigrate/MenuItem.java
+++ trunk/db-migrate/src/main/java/com/googlecode/hdbc/dbmigrate/MenuItem.java
@@ -1,10 +1,26 @@
 package com.googlecode.hdbc.dbmigrate;
 
-public final class MenuItem {
+import java.io.IOException;
 
-    private MenuItem() {}
+public final class MenuItem {
+    private final String question;
+    private final Menu subMenu;
+
+    private MenuItem(final String topicQuestion, final Menu menu) {
+        this.question = topicQuestion;
+        this.subMenu = menu;
+    }
 
     public static MenuItem item(final String question, final Menu subMenu) {
-        return new MenuItem();
+        return new MenuItem(question, subMenu);
+    }
+
+    public void runSubMenu() throws IOException {
+        subMenu.run();
+    }
+
+    @Override
+    public String toString() {
+        return question;
     }
 }
