7a9e6bba786a65b6c0076815f420278e8d854bdb
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-06-18 03:49:31 +0000

    tracer code

--- hippo/src/main/java/com/googlecode/hdbc/controller/ExperimentsController.java
+++ hippo/src/main/java/com/googlecode/hdbc/controller/ExperimentsController.java
@@ -1,23 +1,25 @@
 package com.googlecode.hdbc.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.googlecode.hdbc.model.Experiment;
+import com.googlecode.hdbc.dao.IExperimentsDao;
+import com.googlecode.hdbc.model.IExperiment;
 
 @Controller
 @RequestMapping("/experiments/*")
 public class ExperimentsController {
+	private IExperimentsDao data;
+	
+	public ExperimentsController(IExperimentsDao dao) {
+		data = dao;
+	}
 
 	@RequestMapping(value="index", method=RequestMethod.GET)
 	public void index(Model model) {
-		List<Experiment> experiments = new ArrayList<Experiment>();
-		experiments.add(new Experiment("exp1", "to test stuff"));
-		experiments.add(new Experiment("sign up experiment","to test ui elements for their influence on sign up"));
+		List<IExperiment> experiments = data.findActiveExperiments();
 		model.addAttribute("experiments", experiments);
 	}
 	
