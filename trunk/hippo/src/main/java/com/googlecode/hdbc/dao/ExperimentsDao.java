b4997b898ca1566b75b84c1edfbc390df0be5262
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-06-18 03:52:15 +0000

    start tracer components

---
+++ trunk/hippo/src/main/java/com/googlecode/hdbc/dao/ExperimentsDao.java
@@ -0,0 +1,14 @@
+package com.googlecode.hdbc.dao;
+
+import java.util.List;
+
+import com.googlecode.hdbc.model.IExperiment;
+
+public class ExperimentsDao implements IExperimentsDao {
+
+	@Override
+	public List<IExperiment> findActiveExperiments() {
+		return null;
+	}
+
+}
---
+++ trunk/hippo/src/main/java/com/googlecode/hdbc/model/IExperiment.java
@@ -0,0 +1,5 @@
+package com.googlecode.hdbc.model;
+
+public interface IExperiment {
+
+}
