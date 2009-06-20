be420321d67fd1f4953818931a103d4a38dfdfc8
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-06-20 09:46:37 +0000

    integration for spring 3.0

--- trunk/hippo/src/main/java/com/googlecode/hdbc/controller/ExperimentController.java
+++ trunk/hippo/src/main/java/com/googlecode/hdbc/controller/ExperimentController.java
@@ -1,16 +1,27 @@
 package com.googlecode.hdbc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import com.googlecode.hdbc.dao.IExperimentDao;
+import com.googlecode.hdbc.model.IExperiment;
 
 @Controller
-@RequestMapping("/experiment/*")
+@RequestMapping("/experiment/{uid}")
 public class ExperimentController {
+	IExperimentDao dao;
+	
+	public ExperimentController(IExperimentDao exprmntDao) {
+		dao = exprmntDao;
+	}
 
 	@RequestMapping(method=RequestMethod.GET)
-	void get() {
-		
+	public String get(@PathVariable("uid") long uid, Model model) {
+		IExperiment experiment = dao.find(uid);
+		model.addAttribute("experiment", experiment);
+		return "experiment/view";
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
