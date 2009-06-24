2dedc7c9dcc0387bda07cbf39ce728c611ef785c
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-06-24 12:57:47 +0000

    begin implementation of error handling

--- hippo/src/main/webapp/WEB-INF/jsp/experiments.jsp
+++ hippo/src/main/webapp/WEB-INF/jsp/experiments.jsp
@@ -9,20 +9,32 @@
 			<li>
 				<label for="name">Title:<em>*</em></label>
 				<form:input id="title" path="title" />
+				<div class="error">
+					<form:errors path="title" />
+				</div>
 			</li>
 			<li>
 				<label for="purpose">Purpose:<em>*</em></label>
 				<form:textarea id="purpose" path="purpose"/>
+				<div class="error">
+					<form:errors path="purpose" />
+				</div>
 			</li>
 			<li>
 				<label for="method">Method:</label>
 				<form:textarea id="method" path="method" />
+				<div class="error">
+					<form:errors path="method" />
+				</div>
 			</li>
 			<li>
 				<label for="conclusion">Conclusion:</label>
 				<form:textarea id="conclusion" path="conclusion" />
+				<div class="error">
+					<form:errors path="method" />
+				</div>
 			</li>
 		</ol>
-		<input type="submit" name="submit" value="submit" />
+		<input type="submit" name="submit" value="${empty experiment.uid ? 'create' : 'update'}" />
 	</fieldset>
 </form:form>
--- hippo/src/test/java/com/googlecode/hdbc/controller/ExperimentsControllerTest.java
+++ hippo/src/test/java/com/googlecode/hdbc/controller/ExperimentsControllerTest.java
@@ -9,16 +9,6 @@ import org.springframework.validation.DataBinder;
 
 import com.googlecode.hdbc.model.record.ExperimentData;
 
-/**
- * Experiment Controller tests
- * <p>
- * binding specification
- * bind :: Request -> DataBinder
- * (1) bind map | map(keys E {fields}) = DataBinder
- * (2) 			| map(keys E ({fields} - {required fields}) = DataBinder(error)
- * (3) 			| map(keys E ({required fields} + {!fields}) = DataBinder
- * 
- */
 public class ExperimentsControllerTest {
 	private MockHttpServletRequest req;
 	private ExperimentsController ctlr;
@@ -32,61 +22,6 @@ public class ExperimentsControllerTest {
 		req.setParameter("method", "very good method");
 		
 		ctlr = new ExperimentsController(null, null);
-	}
-	
-	/**
-	 * Test binding all fields - condition (1) (above spec)
-	 */
-	@Test
-	public void testBinding1() {
-		DataBinder actual = ctlr.bind(req);
-		assertNotNull(actual);
-		ExperimentData data = (ExperimentData) actual.getTarget();
-		assertEquals("some name", data.getTitle());
-		assertEquals(23, data.getUid().longValue());
-	}
-	
-	/**
-	 * Test binding condition 2 (above spec)
-	 */
-	@Test
-	public final void testBinding2() {
-		req.removeParameter("title");
-		DataBinder actual = ctlr.bind(req);
-		int count = actual.getBindingResult().getErrorCount();
-		assertEquals(1, count);
-	}
-	
-	/**
-	 * Test binding condition 2 for when name exists as a key but empty(value) == true
-	 */
-	@Test
-	public final void testBinding2EmptyValue() {
-		req.removeParameter("title");
-		DataBinder actual = ctlr.bind(req);
-		int count = actual.getBindingResult().getErrorCount();
-		assertEquals(1, count);
-	}
-	
-	/**
-	 * Test binding condition 3 (above spec) 
-	 */
-	@Test
-	public final void testBinding3() {
-		req.addParameter("submit", "submit");
-		DataBinder actual = ctlr.bind(req);
-		int count = actual.getBindingResult().getErrorCount();
-		assertEquals(0, count);
-	}
-	
-	/**
-	 * Test that the bound fields are trimmed
-	 */
-	@Test
-	public final void testBoundFieldsHaveTrimmedValues() {
-		req.addParameter("conclusion", "  conclusion   ");
-		DataBinder actual = ctlr.bind(req);
-		ExperimentData data = (ExperimentData) actual.getTarget();
-		assertEquals("conclusion", data.getConclusion());
-	}
+	}	
+
 }
