9b49243e27e72a59f2f54fee974596d70ec61fff
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-06-24 12:57:47 +0000

    begin implementation of error handling

--- trunk/hippo/src/main/java/com/googlecode/hdbc/controller/ExperimentsController.java
+++ trunk/hippo/src/main/java/com/googlecode/hdbc/controller/ExperimentsController.java
@@ -4,13 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
 import org.springframework.validation.Validator;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
 import com.googlecode.hdbc.dao.IExperimentDao;
 import com.googlecode.hdbc.model.Experiment;
 import com.googlecode.hdbc.model.IExperiment;
@@ -19,6 +20,7 @@ import com.googlecode.hdbc.model.record.ExperimentData;
 @Controller
 public class ExperimentsController {
 	public static final String COMMAND_NAME = "experiment";
+	public static final String EXPERIMENT_FORM = "experiments";
 	private final IExperimentDao dao;
 	private final Validator validator;
 	
@@ -30,42 +32,34 @@ public class ExperimentsController {
 	@RequestMapping(value="/experiments", method=RequestMethod.GET)
 	public String newExperiment(final Model model) {
 		model.addAttribute(COMMAND_NAME, new ExperimentData());
-		return "experiments";
+		return EXPERIMENT_FORM;
 	}
 	
 	@RequestMapping(value="/experiments/{uid}", method=RequestMethod.GET)
-	public String get(@PathVariable("uid") final long uid, final Model model) {
+	public String getExperiment(@PathVariable("uid") final long uid, final Model model) {
 		IExperiment experiment = dao.find(uid);
 		model.addAttribute(COMMAND_NAME, experiment.getData());
-		return "experiments";
+		return EXPERIMENT_FORM;
 	}
 	
 	@RequestMapping(value="/experiments", method=RequestMethod.POST)
-	public String create(final Model model, final HttpServletRequest req) {
-		DataBinder binder = bind(req);
-		ExperimentData data = (ExperimentData) binder.getTarget();
-		validator.validate(data, binder.getBindingResult());
-		
-		String viewName = "experiments";
-		model.addAttribute(COMMAND_NAME, data);
-		if (binder.getBindingResult().getErrorCount() == 0) {
+	public String createExperiment(@ModelAttribute(COMMAND_NAME) final ExperimentData data, final BindingResult result) {
+		validator.validate(data, result);
+
+		if (!result.hasErrors()) {
 			dao.insert(new Experiment(data));
-			viewName += "/" + data.getUid().toString();
-		} else {
-			model.addAttribute("errors", binder.getBindingResult());
 		}
-		return viewName;
+		return EXPERIMENT_FORM;
 	}
 	
 	@RequestMapping(value="/experiments/{uid}", method=RequestMethod.PUT)
-	void update(@PathVariable("uid") final long uid) {
+	public String updateExperiment(@ModelAttribute(COMMAND_NAME) final ExperimentData data, final BindingResult result) {
+		validator.validate(data, result);
 		
+		if (!result.hasErrors()) {
+			dao.update(new Experiment(data));
+		}
+		return EXPERIMENT_FORM;
 	}
-	
-	protected final DataBinder bind(final HttpServletRequest req) {
-		ServletRequestDataBinder binder = new ServletRequestDataBinder(new ExperimentData());
-		binder.setRequiredFields(new String[] {"title", "purpose"});
-		binder.bind(req);
-		return binder;
-	}
+
 }
