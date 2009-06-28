3e281107323d3221b291dfc1797fc2a5f903b3eb
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-06-28 14:34:16 +0000

    added message bundles for localization

--- hippo/src/test/java/com/googlecode/hdbc/controller/ExperimentsControllerTest.java
+++ hippo/src/test/java/com/googlecode/hdbc/controller/ExperimentsControllerTest.java
@@ -1,27 +1,13 @@
 package com.googlecode.hdbc.controller;
 
 import static org.junit.Assert.*;
-
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.validation.DataBinder;
-
-import com.googlecode.hdbc.model.record.ExperimentData;
 
 public class ExperimentsControllerTest {
-	private MockHttpServletRequest req;
-	private ExperimentsController ctlr;
 	
-	@Before
-	public final void setUp() {
-		req = new MockHttpServletRequest();
-		req.setParameter("uid", "23");
-		req.setParameter("title", "some name");
-		req.setParameter("purpose", "to test values");
-		req.setParameter("method", "very good method");
-		
-		ctlr = new ExperimentsController(null, null);
-	}	
+	@Test
+	public void testNothing() {
+		assertTrue(true);
+	}
 
 }
