aa836efef3fa91f29fd575ee21bc3877266ec168
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-06-29 02:04:32 +0000

    allow for compilation with java 5

--- trunk/hippo/src/main/java/com/googlecode/hdbc/dao/ExperimentDao.java
+++ trunk/hippo/src/main/java/com/googlecode/hdbc/dao/ExperimentDao.java
@@ -4,17 +4,14 @@ import com.googlecode.hdbc.model.IExperiment;
 
 public class ExperimentDao implements IExperimentDao {
 
-	@Override
 	public IExperiment find(long uid) {
 		return null;
 	}
 
-	@Override
 	public long insert(IExperiment experiment) {
 		return 0;
 	}
 
-	@Override
 	public void update(IExperiment experiment) {
 		
 	}
