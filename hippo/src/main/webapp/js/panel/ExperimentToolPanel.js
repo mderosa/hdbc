d94169ce9318205875c27e5be4d1c495bf9e19eb
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-07-14 15:07:22 +0000

    trying to get event propogation working

--- hippo/src/main/webapp/js/panel/ExperimentToolPanel.js
+++ hippo/src/main/webapp/js/panel/ExperimentToolPanel.js
@@ -4,17 +4,37 @@ Ext.namespace('Hippo.panel');
 Hippo.panel.ExperimentToolPanel = Ext.extend(Ext.Toolbar, {
 	
 	initComponent: function() {
+
 		Ext.apply(this, {
 			id: 'toolbar'
 			,items: [{
-				text: 'New*'
+				text: 'Hippo'
+				,xtype: 'tbtext'
+			}
+		   ,{
+				xtype: 'tbseparator'
+			}
+		   ,{
+				id: 'newExperiment'
+				,text: 'New*'
 				,xtype: 'tbbutton'
 				,tooltip: 'create a new experiment'
 				,style: 'border: solid 1px #AAA'
+				,listeners: {
+					click: {
+						fn: function(e, t) {
+							alert('button');
+							this.fireEvent('newexperiment');
+						},
+						scope: this
+					}
+				}
 			}]
 		});
 		
 		Hippo.panel.ExperimentToolPanel.superclass.initComponent.apply(this, arguments);
+		
+		this.addEvents('newexperiment');
 	}
 
 });
