5e5d0c8d2cd1e0d4fcf73be97fe1a3f3aef9e6de
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-07-14 11:19:27 +0000

    make purpose a not null field

--- trunk/hippo-migrate/do/00008-do_CreateExperiments.sql
+++ trunk/hippo-migrate/do/00008-do_CreateExperiments.sql
@@ -10,7 +10,7 @@ CREATE TABLE hippo.experiments(
 	experiment_cd 		number CONSTRAINT pk_experiments PRIMARY KEY,
 	type 				varchar2(16 char) NOT NULL,
 	title				varchar2(64 char) NOT NULL,
-	purpose				varchar2(64 char),
+	purpose				varchar2(128 char) NOT NULL,
 	method				varchar2(4000 char),
 	conclusion			varchar2(4000 char)
 );
