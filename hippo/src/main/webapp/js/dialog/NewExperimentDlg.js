2417f0d703a355760455d32ed972d038a376141c
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-07-22 09:05:23 +0000

    fix up the formatting and code content

--- hippo/src/main/webapp/js/dialog/NewExperimentDlg.js
+++ hippo/src/main/webapp/js/dialog/NewExperimentDlg.js
@@ -3,15 +3,7 @@ Ext.namespace('Hippo.dialog');
 
 Hippo.dialog.NewExperimentDlg = Ext.extend(Ext.Window, {
 	
-	onTextFieldClick: function() {
-		console.log('begin on text field click');
-		var hasContent = function(id) {
-			return (Ext.get(id).getValue().length > 0);
-		}
-		var form = this.getComponent(0);
-		form.createButtonActive(hasContent('title') && hasContent('purpose'));
-	}
-	,initComponent: function(config) {
+	initComponent: function(config) {
 		Ext.apply(this, {
 			id: 'wndnewexpmnt'
 			,title: 'New Experiment'
@@ -22,7 +14,6 @@ Hippo.dialog.NewExperimentDlg = Ext.extend(Ext.Window, {
 			}]
 		});
 		Hippo.dialog.NewExperimentDlg.superclass.initComponent.apply(this, arguments);
-		this.relayEvents(this.getComponent(0), ['newexpformchange']);
-		this.on('newexpformchange', this.onTextFieldClick, this);
+
 	}
 });
