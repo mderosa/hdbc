e0b91e4929b3c18b13df949ce5f7629c1354a529
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-07-25 00:18:44 +0000

    validation

--- trunk/hippo/src/main/java/com/googlecode/hdbc/model/validator/ExperimentDataValidator.java
+++ trunk/hippo/src/main/java/com/googlecode/hdbc/model/validator/ExperimentDataValidator.java
@@ -1,13 +1,19 @@
 package com.googlecode.hdbc.model.validator;
 
-import org.springframework.validation.Errors;
-import com.googlecode.hdbc.model.record.ExperimentData;
+import java.util.Locale;
 
+import org.springframework.context.MessageSource;
+
+import com.googlecode.hdbc.model.record.ExperimentData;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 public class ExperimentDataValidator extends DataValidator implements Validator<ExperimentData> {
 
+	public ExperimentDataValidator(MessageSource msgSource) {
+		super(msgSource);
+	}
+	
 	public boolean supports(Class<?> clazz) {
 		return false;
 	}
@@ -31,23 +37,28 @@ public class ExperimentDataValidator extends DataValidator implements Validator<
 	 * precondition is obj.title and obj.purpose /= null
 	 * postcondition error.count >= 0
 	 */
-	public JSONObject validate(ExperimentData data) {
+	public JSONObject validate(ExperimentData data, Locale locale) {
 		JSONArray errors = new JSONArray();
-		/*if (!this.isNullOrGreaterThan(data.getUid(), 0)) {
-			errors.rejectValue("uid", ValidationErrorCd.NUMBER_BELOW_MINIMUM.toString(), new Object[] {0}, "na");
+		if (!this.isNullOrGreaterThan(data.getUid(), 0)) {
+			errors.add(
+				rejectValue("uid", ValidationErrorCd.NUMBER_BELOW_MINIMUM.toString(), new Object[] {0}, locale));
 		}
 		if (!this.isNotNullAndUpToLengthN(data.getTitle(), 64)) {
-			errors.rejectValue("title", ValidationErrorCd.REQUIRED_STRING_TOO_LONG.toString(), new Object[] {64}, "na");
+			errors.add(
+				rejectValue("title", ValidationErrorCd.REQUIRED_STRING_TOO_LONG.toString(), new Object[] {64}, locale));
 		}
 		if (!this.isNotNullAndUpToLengthN(data.getPurpose(), 128)) {
-			errors.rejectValue("purpose", ValidationErrorCd.REQUIRED_STRING_TOO_LONG.toString(), new Object[] {128}, "na");
+			errors.add(
+				rejectValue("purpose", ValidationErrorCd.REQUIRED_STRING_TOO_LONG.toString(), new Object[] {128}, locale));
 		}
 		if (!this.isNullOrUptoLengthN(data.getMethod(), 4000)) {
-			errors.rejectValue("method", ValidationErrorCd.OPTIONAL_STRING_TO_LONG.toString(), new Object[] {4000}, "na");
+			errors.add(
+				rejectValue("method", ValidationErrorCd.OPTIONAL_STRING_TO_LONG.toString(), new Object[] {4000}, locale));
 		}
 		if (!this.isNullOrUptoLengthN(data.getConclusion(), 4000)) {
-			errors.rejectValue("conclusion", ValidationErrorCd.OPTIONAL_STRING_TO_LONG.toString(), new Object[] {4000}, "na");
-		}*/
+			errors.add(
+				rejectValue("conclusion", ValidationErrorCd.OPTIONAL_STRING_TO_LONG.toString(), new Object[] {4000}, locale));
+		}
 		
 		JSONObject jsn = new JSONObject();
 		if (errors.size() > 0) {
--- trunk/hippo/src/main/java/com/googlecode/hdbc/model/validator/Validator.java
+++ trunk/hippo/src/main/java/com/googlecode/hdbc/model/validator/Validator.java
@@ -1,10 +1,12 @@
 package com.googlecode.hdbc.model.validator;
 
+import java.util.Locale;
+
 import net.sf.json.JSONObject;
 
 public interface Validator<T> {
 	
 	boolean supports(Class<?> clazz);
 	
-	JSONObject validate(T input);
+	JSONObject validate(T input, Locale locale);
 }
