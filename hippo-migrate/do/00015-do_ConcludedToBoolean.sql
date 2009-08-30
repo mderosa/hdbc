5dd9573be121e72435a22e78a4f016e0e4e65d78
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-08-30 14:13:34 +0000

    get experiments table in shape

---
+++ hippo-migrate/do/00015-do_ConcludedToBoolean.sql
@@ -0,0 +1,20 @@
+VARIABLE v_pk  NUMBER;
+
+BEGIN
+    hippo.sp_log_version_change_begin('00015-do_ConcludedToBoolean.sql', &1, :v_pk);
+END;
+/
+
+---- ddl below ------------------------------------------
+ALTER TABLE hippo.experiments DROP (concluded);
+ALTER TABLE hippo.experiments ADD (concluded NUMBER(1) NOT NULL);
+---------------------------------------------------------
+
+BEGIN
+    hippo.sp_update_version(15);
+    hippo.sp_log_version_change_finish(:v_pk, 'success');
+EXCEPTION
+    WHEN OTHERS THEN
+        hippo.sp_log_version_change_finish(:v_pk, 'fail');
+END;
+/
