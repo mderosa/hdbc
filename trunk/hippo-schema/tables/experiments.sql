30673388c786e7e2c25bd50cd4daa9e40fa65f1f
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-08-30 01:31:28 +0000

    for now make type nullable

--- trunk/hippo-schema/tables/experiments.sql
+++ trunk/hippo-schema/tables/experiments.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE experiments(
 	experiment_cd 		number CONSTRAINT pk_experiments PRIMARY KEY,
-	type 				varchar2(16 char) NOT NULL,
+	type 				varchar2(16 char),
 	title				varchar2(64 char) NOT NULL,
 	purpose				varchar2(128 char) NOT NULL,
 	method				varchar2(4000 char),
