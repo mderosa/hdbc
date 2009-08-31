64c0f7ed45ae2e0a22b555f30efff5e9727bb9f1
Marc.DeRosa <Marc.DeRosa@8f82134a-c933-0410-9666-67999694cb3c>
2009-08-31 13:39:11 +0000

    view component

---
+++ trunk/hippo/src/main/java/com/googlecode/hdbc/view/policy/ActiveExperimentsOutputPolicy.java
@@ -0,0 +1,13 @@
+package com.googlecode.hdbc.view.policy;
+
+import java.util.Map;
+
+public class ActiveExperimentsOutputPolicy implements ICustomOutputPolicy {
+
+	@Override
+	public Map<String, Object> customOutput(Map<String, Object> model) {
+		
+		return null;
+	}
+
+}
